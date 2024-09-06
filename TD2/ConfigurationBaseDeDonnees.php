e404055df50cd2eb8c67534e169ee03ca180e38c
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-09-06 10:54:50 +0200

    Implémentation de l'exercice 2

--- TD2/ConfigurationBaseDeDonnees.php
+++ TD2/ConfigurationBaseDeDonnees.php
@@ -7,27 +7,42 @@ class ConfigurationBaseDeDonnees {
         //
         // ou webinfo.iutmontp.univ-montp2.fr
         // pour accéder à webinfo depuis l'extérieur
-        'nomHote' => 'a_remplir',
+        'nomHote' => 'webinfo.iutmontp.univ-montp2.fr',
         // A l'IUT, vous avez une base de données nommee comme votre login
         // Sur votre machine, vous devrez creer une base de données
-        'nomBaseDeDonnees' => 'a_remplir',
+        'nomBaseDeDonnees' => 'franceusm',
         // À l'IUT, le port de MySQL est particulier : 3316
         // Ailleurs, on utilise le port par défaut : 3306
-        'port' => 'a_remplir',
+        'port' => '3316',
         // A l'IUT, c'est votre login
         // Sur votre machine, vous avez surement un compte 'root'
-        'login' => 'a_remplir',
+        'login' => 'franceusm',
         // A l'IUT, c'est le même mdp que PhpMyAdmin
         // Sur votre machine personelle, vous avez creez ce mdp a l'installation
-        'motDePasse' => 'a_remplir'
+        'motDePasse' => 'Upxi2kThgmgNSSu'
     );
 
-    static public function getLogin() : string {
+    static public function getLogin(): string {
         // L'attribut statique $configurationBaseDeDonnees
         // s'obtient avec la syntaxe ConfigurationBaseDeDonnees::$configurationBaseDeDonnees
         // au lieu de $this->configurationBaseDeDonnees pour un attribut non statique
         return ConfigurationBaseDeDonnees::$configurationBaseDeDonnees['login'];
     }
 
+    public static function getNomHote(): string {
+        return ConfigurationBaseDeDonnees::$configurationBaseDeDonnees['nomHote'];
+    }
+
+    public static function getPort(): int {
+        return intval(ConfigurationBaseDeDonnees::$configurationBaseDeDonnees['port']);
+    }
+
+    public static function getNomBaseDeDonnees(): string {
+        return ConfigurationBaseDeDonnees::$configurationBaseDeDonnees['nomBaseDeDonnees'];
+    }
+
+    public static function getPassword(): string {
+        return ConfigurationBaseDeDonnees::$configurationBaseDeDonnees['motDePasse'];
+    }
 }
 ?>

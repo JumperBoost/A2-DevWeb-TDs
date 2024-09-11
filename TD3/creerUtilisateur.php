88dac160515afc5b0a337a4007b26cb9661f2b5a
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-09-11 14:23:38 +0200

    feat: Implémentation de l'exercice 3

---
+++ TD3/creerUtilisateur.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'Utilisateur.php';
+
+$login = $_POST['login'];
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+
+$utilisateur = new Utilisateur($login, $nom, $prenom);
+$utilisateur->ajouter();

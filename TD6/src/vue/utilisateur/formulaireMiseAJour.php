976b890b4e9605b4943d7a1be0b229ece15b484f
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-10-04 11:30:43 +0200

    fix: Utilisation de htmlspecialchars pour le formulaire de mise à jour utilisateur (oubli)

--- TD6/src/vue/utilisateur/formulaireMiseAJour.php
+++ TD6/src/vue/utilisateur/formulaireMiseAJour.php
@@ -9,15 +9,15 @@
         <legend>Formulaire de mise à jour :</legend>
         <p class="InputAddOn">
             <label class="InputAddOn-item" for="login_id">Login</label>
-            <input class="InputAddOn-field" type="text" name="login" id="login_id" value="<?= $utilisateur->getLogin() ?>" readonly required/>
+            <input class="InputAddOn-field" type="text" name="login" id="login_id" value="<?= htmlspecialchars($utilisateur->getLogin()) ?>" readonly required/>
         </p>
         <p class="InputAddOn">
             <label class="InputAddOn-item" for="nom_id">Nom</label>
-            <input class="InputAddOn-field" type="text" placeholder="Leblanc" name="nom" id="nom_id" value="<?= $utilisateur->getNom() ?>" required/>
+            <input class="InputAddOn-field" type="text" placeholder="Leblanc" name="nom" id="nom_id" value="<?= htmlspecialchars($utilisateur->getNom()) ?>" required/>
         </p>
         <p class="InputAddOn">
             <label class="InputAddOn-item" for="prenom_id">Prénom</label>
-            <input class="InputAddOn-field" type="text" placeholder="Juste" name="prenom" id="prenom_id" value="<?= $utilisateur->getPrenom() ?>" required/>
+            <input class="InputAddOn-field" type="text" placeholder="Juste" name="prenom" id="prenom_id" value="<?= htmlspecialchars($utilisateur->getPrenom()) ?>" required/>
         </p>
         <p class="InputAddOn">
             <input class="InputAddOn-item" type="submit" value="Envoyer" />
